f6fe750ea771e9713a0ada42145e484e8ce6357f
Bami Idowu <bami.idowu@yahoo.co.uk>
2016-05-17 17:11:43 +0100

    Add Javadocs

--- src/main/java/com/boostrdev/spring/retrofit2/WebServiceProxyFactory.java
+++ src/main/java/com/boostrdev/spring/retrofit2/WebServiceProxyFactory.java
@@ -11,6 +11,13 @@ import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.SocketAddress;
 
+/**
+ * This factory class creates an instance of Interface class
+ * which provides client/server interactions.
+ * The interface should be annotated with Retrofit annotations
+ * and should not extend/implement any other class/interface.
+ */
+
 public final class WebServiceProxyFactory {
 
     private static final Logger LOG = LoggerFactory.getLogger(WebServiceProxyFactory.class);
@@ -22,14 +29,15 @@ public final class WebServiceProxyFactory {
     /**
      * Create an implementation of the API endpoints defined by the {@code webServiceProxy} interface.
      *
-     * @param webServiceProxyClazz
-     * @param converterFactory
-     * @param baseUrl
-     * @param proxyHost
-     * @param proxyPort
-     * @param proxy
-     * @param logging
-     * @return
+     * @param <T>                  The web proxy interface type
+     * @param webServiceProxyClazz The web proxy interface class
+     * @param converterFactory     The JSON converterFactory
+     * @param baseUrl              The base location of the web service
+     * @param proxyHost            The host name of the HTTP proxy
+     * @param proxyPort            The port number of the HTTP proxy
+     * @param proxy                Whether a HTTP proxy should be used to send HTTP requests and responses
+     * @param logging              Whether the HTTP logging should be turned on
+     * @return Returns a Retrofit instance of the web service proxy using
      */
     public static <T> T create(Class<T> webServiceProxyClazz, Converter.Factory converterFactory, String baseUrl, String proxyHost, int proxyPort, boolean proxy, boolean logging) {
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
